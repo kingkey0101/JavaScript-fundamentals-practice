ef76c064c677ea761a8c9eaaf14d6699d85970e6
Keylan King <kingkey0101@outlook.com>
2025-12-02 17:52:33 -0600

    Moving forward

--- FES Advanced Challenges/questions/stringifyId/index.js
+++ FES Advanced Challenges/questions/stringifyId/index.js
@@ -16,8 +16,87 @@
  * ]) === 'ABC, abc, CBA'
  */
 
-const stringifyId = (arr) => {
+//1st. convert the array of objects into an array of IDs:
+// const stringifyId = (arr) => {
+// const idArray = arr.map(obj => obj.id);
+// return idArray
+// };
 
+// 2nd. convert the array of ID's into an array of unique ID's(no duplicates): (first way: brute force):
+// const stringifyId = (arr) => {
+// const idArray = arr.map(obj => obj.id);
+// const uniqueArray = [];
+// let idString = '';
+// for (let id of idArray) {
+//     if(!uniqueArray.includes(id)) {
+//         uniqueArray.push(id)
+//     }
+// return uniqueArray
+// }
+// 3rd. join the array to create a string of IDs separated by ", "
+// (first way: brute force):
+
+// const stringifyId = (arr) => {
+// const idArray = arr.map(obj => obj.id);
+// const uniqueArray = [];
+// let idString = '';
+// for (let id of idArray) {
+//     if(!uniqueArray.includes(id)) {
+//         uniqueArray.push(id)
+//     }
+// }
+// for(let i =0; i < uniqueArray.length; ++i){
+//     // if not on the last element
+//     if(i !== uniqueArray.length - 1) {
+//         idString += `${uniqueArray[i]}, `
+//     }
+//     else {
+//         idString += uniqueArray[i]
+//     }
+//     //  ad the id and ", "
+//     //  otherwise just add the id
+
+// }
+// return idString
+// };
+
+// (second way(3rd step): top tech):
+
+// const stringifyId = (arr) => {
+// const idArray = arr.map(obj => obj.id);
+// const uniqueArray = [];
+// let idString = '';
+// for (let id of idArray) {
+//     if(!uniqueArray.includes(id)) {
+//         uniqueArray.push(id)
+//     }
+// }
+// for(let i =0; i < uniqueArray.length; ++i){
+//     idString += `${uniqueArray[i]}, `;
+
+// }
+// return idString.slice(0, idString.length -2)
+// };
+
+// (second way: top tech even easier): .join()
+
+// const stringifyId = (arr) => {
+// const idArray = arr.map(obj => obj.id);
+// const uniqueArray = [];
+// for (let id of idArray) {
+//     if(!uniqueArray.includes(id)) {
+//         uniqueArray.push(id)
+//     }
+// }
+
+// return uniqueArray.join(", ")
+// };
+
+// (second way: top tech in 3 lines of code using .set):
+const stringifyId = (arr) => {
+const idArray = arr.map(obj => obj.id);
+const uniqueArray = [...new Set(idArray)];
+return uniqueArray.join(", ")
 };
 
 module.exports = stringifyId;
