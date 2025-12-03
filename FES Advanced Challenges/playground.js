dca900cbe50d380b8060f2e510f91fca39c027c7
Keylan King <kingkey0101@outlook.com>
2025-12-02 20:08:41 -0600

    anagrams

--- FES Advanced Challenges/playground.js
+++ FES Advanced Challenges/playground.js
@@ -1,40 +1,4 @@
-//first solution: good for basic junior roles
-// const str = "Simplified !";
-// const charMap = {};
-// for (let char of str) {
-//   if (!charMap[char]) {
-//     charMap[char] = 1;
-//   }
-//   else {
-//     charMap[char] = charMap[char] +1;
-//   }
-// }
-// console.log(charMap);
-//second solution: good for better tech roles
-// const str = "Simplified !";
-// const charMap = {};
-// for (let char of str) {
-//     charMap[char] = charMap[char] +1 || 1;
-// }
-// console.log(charMap);
+const strA = 'cat'
+const strB = 'act'
 
-//third solution: good for top tech roles
-// const str = "Simplified !";
-// const charMap = {};
-// for (let char of str) {
-//     charMap[char] = charMap[char] +1 || 1;
-// }
-// for (let char in charMap){
-
-//     console.log(charMap[char]);
-// }
-
-
-const str = "Simplified !"
-const charMap = new Map();
-
-for (let char of str){
-    const count = charMap.get(char)
-    charMap.set(char, count + 1 || 1)
-}
-console.log(charMap)
+console.log(strA.split('').sort().join('') === strB.split('').sort().join(''))
--- FES Advanced Challenges/questions/anagrams/index.js
+++ FES Advanced Challenges/questions/anagrams/index.js
@@ -9,8 +9,43 @@
  * anagrams('frontend', 'simplified') === false
  */
 
-const anagrams = (strA, strB) => {
-  
-}
+// const anagrams = (strA, strB) => {
+//   const charMapA = buildCharMap(strA)
+//   const charMapB = buildCharMap(strB)
 
+//MENTION THIS EDGE CASE:
+//   if (charMapA.size !== charMapB.size) {
+//     return false;
+//   }
+
+//   for(const [charA, countA] of charMapA) {
+//     if (charMapB.get(charA) !== countA) {
+//         return false;
+//     }
+//   }
+//   return true;
+
+// }
+
+// const buildCharMap = (str)=>{
+//     const charMap = new Map()
+//  for (const char of removeSpacesAndLowerCase(str)){
+//     charMap.set(char, charMap.get(char) +1 || 1)
+//   }
+//   return charMap
+// }
+// const removeSpacesAndLowerCase = (str) => {
+//     return str.toLowerCase().replaceAll(' ', '')
+// }
+
+//Top tech: solve in two lines of code
+const anagrams = (strA, strB) => {
+//set both to lowercase
+//remove all the spaces
+// sorting the strings: array sort() method:
+return cleanString(strA) === cleanString(strB)
+}
+const cleanString = (str) => {
+ return str.toLowerCase().replaceAll(' ', '').split('').sort().join('')
+}
 module.exports = anagrams;
