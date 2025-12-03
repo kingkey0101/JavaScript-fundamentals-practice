9358a1fdd4a0696beb9562505e9c75d7e3e96a8a
Keylan King <kingkey0101@outlook.com>
2025-12-03 16:54:46 -0600

    Add files via upload

---
+++ Todo List Starter/app.js
@@ -0,0 +1,45 @@
+const list = document.querySelector(".list");
+const input = document.querySelector("input");
+
+let todoList = [];
+let todoInputValue = "";
+let counter = 0;
+
+function onInputChange(event) {
+  todoInputValue = event.target.value;
+}
+function addTodo() {
+  if (!todoInputValue) {
+    return;
+  }
+
+  todoList.push({
+    id: counter++,
+    task: todoInputValue,
+  });
+  console.log(todoList)
+  renderTodos();
+  input.value = "";
+  todoInputValue = "";
+}
+
+function deleteTodo(id) {
+  //   console.log(id);
+  todoList = todoList.filter((todo) => todo.id !== id);
+  renderTodos();
+}
+
+function renderTodos() {
+  list.innerHTML = todoList
+    .map(
+      (element) =>
+        `<li>
+            ${element.task}
+            <button class="todo__delete" onclick="deleteTodo(${element.id})">
+            x
+            </button>
+        </li>
+`
+    )
+    .join("");
+}
