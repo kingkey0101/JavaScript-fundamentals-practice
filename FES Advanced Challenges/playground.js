91b4cf797187da52de6345170226896cdc961597
Keylan King <kingkey0101@outlook.com>
2025-12-03 12:50:05 -0600

    steps

--- FES Advanced Challenges/playground.js
+++ FES Advanced Challenges/playground.js
@@ -1,6 +1,15 @@
-const str = 'frontend'
-
-const firstLetter = str[0].toUpperCase()
-const restOfString = str.slice(1)
-
-console.log(firstLetter, restOfString)
+const n = 4;
+const steps = (n) => {
+    for (let row =0; row < n; ++row) {
+        let step = ""
+        for (let column = 0; column < n; ++column) {
+            if (column <= row){
+                step += "#"
+            }
+            else{
+                step += " "
+            }
+        }
+        console.log(step)
+    }
+};
