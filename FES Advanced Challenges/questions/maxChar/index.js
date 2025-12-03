dfa79f900a2e9ff7bf198078717d35ecbebc0ecb
Keylan King <kingkey0101@outlook.com>
2025-12-02 19:04:00 -0600

    maxchar = charMap

--- FES Advanced Challenges/questions/maxChar/index.js
+++ FES Advanced Challenges/questions/maxChar/index.js
@@ -7,7 +7,42 @@
  */
 
 const maxCharacter = (str) => {
-  
+  //create character map
+//   const charMap = {};
+//   let max = 0
+//   let maxChar = ''
+//   for (let char of str){
+//     charMap[char] = charMap[char] + 1 || 1
+//   }
+// //   return charMap
+//   //loop over every char in the character map
+//   for(let char in charMap){
+//     if (charMap[char] > max){
+//         max = charMap[char];
+//         maxChar = char;
+//     }
+//   }
+//   return maxChar;
+    //if the value of hte char is greater than max(initially set this to 0)
+    //set max to the value
+    //set maxChar to the character
+
+ // return maxChar 
+
+ //top tech version:
+   const charMap = new Map()
+  let max = 0
+  let maxChar = ''
+  for (const char of str){
+    charMap.set(char, charMap.get(char) + 1 || 1)
+  }
+  for(const [char, count] of charMap){
+    if (count > max){
+        max = count;
+        maxChar = char;
+    }
+  }
+  return maxChar;
 };
 
 module.exports = maxCharacter;
